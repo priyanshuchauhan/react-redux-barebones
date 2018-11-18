48fdff4908e9810d52b8bfb85fec2d6917f37b2b
priyanshu <js@priyanshuchauhan.com>
2018-11-19 00:04:54 +0530

    Updates

---
+++ precache-manifest.767b2e63f19ad6fb5338ab216afc4a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "019a86afc6cc547bec18",
+    "url": "/react-redux-barebones/static/js/runtime~main.019a86af.js"
+  },
+  {
+    "revision": "bef254ae9b07eabf8ccb",
+    "url": "/react-redux-barebones/static/js/main.bef254ae.chunk.js"
+  },
+  {
+    "revision": "d16a9b3d79e0ec1fbbc4",
+    "url": "/react-redux-barebones/static/js/1.d16a9b3d.chunk.js"
+  },
+  {
+    "revision": "bef254ae9b07eabf8ccb",
+    "url": "/react-redux-barebones/static/css/main.f8af7b91.chunk.css"
+  },
+  {
+    "revision": "71acef3be8cae0771dd66177c78e4688",
+    "url": "/react-redux-barebones/index.html"
+  }
+];
